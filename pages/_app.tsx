ccf4a4f35beb18ca451911df71bbf31a17aa94f9
pandongyuan <pandongyuan@aspirecn.com>
2024-02-27 16:13:11 +0800

    fix: 修改一些信息

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,32 @@
-import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import { IconPack, config, library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import 'windi.css'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import BlogLayout from '../components/layout/BlogLayout'
-import Head from 'next/head'
+import "windi.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import BlogLayout from "../components/layout/BlogLayout";
+import Head from "next/head";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import type { ReactElement, ReactNode } from "react";
-import { useEffect } from 'react';
-import NextNprogress from 'nextjs-progressbar';
-import type { NextPage } from 'next'
-import { pageview } from '../lib/gtag'
-import { ThemeProvider } from 'next-themes'
+import { useEffect } from "react";
+import NextNprogress from "nextjs-progressbar";
+import type { NextPage } from "next";
+import { pageview } from "../lib/gtag";
+import { ThemeProvider } from "next-themes";
 
 export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
-config.autoAddCss = false
-library.add(fab)
+config.autoAddCss = false;
+library.add(fab as IconPack);
 
 declare global {
   interface Window {
@@ -35,7 +35,6 @@ declare global {
 }
 
 function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
-
   useEffect(() => {
     AOS.init({
       easing: "ease-out-cubic",
@@ -46,34 +45,50 @@ function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
     const handleRouteChange = (url: string) => {
       pageview(url, document.title);
       try {
-        window._hmt.push(['_trackPageview', url]);
-      } catch (e) { }
+        window._hmt.push(["_trackPageview", url]);
+      } catch (e) {}
     };
-    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, []);
 
-  const getLayout = Component.getLayout || ((page: any) => <BlogLayout>{page}</BlogLayout>);
+  const getLayout =
+    Component.getLayout || ((page: any) => <BlogLayout>{page}</BlogLayout>);
 
   return (
     <ThemeProvider attribute="class">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <title>安子璠的个人主页</title>
-        <meta name="description" content="异次元の机智君" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <title>Peter Pan</title>
+        <meta name="description" content="潘冬源" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <NextNprogress color="#ff9500" options={{ showSpinner: false }} />
-      {getLayout(
-        <Component {...pageProps} />
-      )}
+      {getLayout(<Component {...pageProps} />)}
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,25 @@
-import type { GetStaticProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from "next";
 
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-import { getDatabase } from '../lib/notion'
-import PostList from '../components/PostList'
-import { Post } from '../lib/types'
+import { getDatabase } from "../lib/notion";
+import PostList from "../components/PostList";
+import { Post } from "../lib/types";
 
 import { getPlaiceholder } from "plaiceholder";
-import { WidgetMeMedium, WidgetMeSmall } from '../components/widget/WidgetMe'
-import ListLayout from '../components/layout/ListLayout'
-import { WidgetOverViewMedium, WidgetOverViewSmall } from '../components/widget/WidgetOverview'
-import { Media, MediaContextProvider } from '../components/utility/Breakpoints'
-import { NextSeo } from 'next-seo'
-import { useRouter } from 'next/router'
-import { me } from '../config/me'
+import { WidgetMeMedium, WidgetMeSmall } from "../components/widget/WidgetMe";
+import ListLayout from "../components/layout/ListLayout";
+import {
+  WidgetOverViewMedium,
+  WidgetOverViewSmall,
+} from "../components/widget/WidgetOverview";
+import { Media, MediaContextProvider } from "../components/utility/Breakpoints";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
+import { me } from "../config/me";
 
 // type PostResult = QueryDatabaseResponse['results'][number];
 
@@ -25,29 +28,22 @@ import { me } from '../config/me'
 // }
 
 const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
-  const mainPosts = posts.slice(0, 17)
+  const mainPosts = posts.slice(0, 17);
   const router = useRouter();
   const { locale } = router;
-  const description = "异次元de机智君的个人博客"
+  const description = "异次元de机智君的个人博客";
   const featuredImage = {
     url: `${me.site}/static/images/og.png`,
     alt: description,
-}
+  };
   return (
     <>
-    {/* <NextSeo
-        canonical={router.asPath}
-        description={description}
-        openGraph={{
-          description,
-          locale,
-          images: [featuredImage],
-          url: `${router.asPath}`,
-        }}
-      /> */}
       <ListLayout>
-        <MediaContextProvider >
-          <Media greaterThanOrEqual="md" className="grid grid-cols-2 gap-6.5 lg:gap-10">
+        <MediaContextProvider>
+          <Media
+            greaterThanOrEqual="md"
+            className="grid grid-cols-2 gap-6.5 lg:gap-10"
+          >
             <WidgetMeMedium />
             <WidgetOverViewMedium posts={posts} />
           </Media>
@@ -59,23 +55,27 @@ const Home: NextPage<{ posts: Post[] }> = ({ posts }) => {
       </ListLayout>
       <PostList posts={mainPosts} />
     </>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const db = await getDatabase()
+  const db = await getDatabase();
   for (let post of db) {
     if (post) {
       try {
-        post.cover.blurLight = (await getPlaiceholder(post.cover.light, {
-          size: 10,
-        })).base64
-        post.cover.blurDark = (await getPlaiceholder(post.cover.dark, {
-          size: 10,
-        })).base64
+        post.cover.blurLight = (
+          await getPlaiceholder(post.cover.light, {
+            size: 10,
+          })
+        ).base64;
+        post.cover.blurDark = (
+          await getPlaiceholder(post.cover.dark, {
+            size: 10,
+          })
+        ).base64;
       } catch (e) {
-        post.cover.blurLight = ''
-        post.cover.blurDark = ''
+        post.cover.blurLight = "";
+        post.cover.blurDark = "";
       }
     }
   }
@@ -85,7 +85,7 @@ export const getStaticProps: GetStaticProps = async () => {
       posts: db,
     },
     revalidate: 60 * 60,
-  }
-}
+  };
+};
 
-export default Home
+export default Home;
